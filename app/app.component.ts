12769eaa225aeeedd8648c4fa16abfb6047050ac
Chooy <toreke12@gmail.com>
2021-10-22 22:19:24 +0700

    Angular template driven forms

---
+++ app/app.component.ts
@@ -0,0 +1,26 @@
+import { ThrowStmt } from '@angular/compiler';
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  firstName = '';
+  lastName = '';
+  gender = '';
+  address = '';
+  password = '';
+  Last = new FormControl('');
+  Name = new FormControl('');
+  id = new FormControl('');
+  Company = new FormControl('');
+  title = 'userInterface';
+
+
+
+}
+ 
